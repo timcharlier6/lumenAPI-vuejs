f3d7ad6ce6e7499e02bb6de2563fb49575f1ead7
Tim Charlier <timcharlier6@etik.com>
2025-03-07 15:42:41 +0100

    css

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,77 +1,126 @@
 <template>
-	<div class="container">
-		<h1>Todo List</h1>
-		<p v-if="loading">Loading...</p>
-		<ul v-else>
-			<li v-for="todo in todos" :key="todo.id">
-				<input type="checkbox" v-model="todo.completed" @change="updateTodo(todo)" />
-				<span class="todo" :class="{ 'completed': todo.completed }">{{ todo.title }}</span>
-				<button @click="deleteTodo(todo.id)">❌</button>
-			</li>
-		</ul>
-		<div class="add-todo">
-			<input type="text" v-model="newTodoTitle" placeholder="Add new todo" />
-			<button @click="addTodo">Add</button>
-		</div>
+  <div>
+	<div class="header" id="myDIV">
+
+    <h1>Todo List</h1>
+
+    <input type="text" id="myInput" v-model="newTodoTitle" placeholder="Title..." />
+    <span class="addBtn" @click="addTodo">Add</span>
 	</div>
+    <p v-if="loading">Loading...</p>
+    <ul id="myUL" v-else>
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        :data-id="todo.id"
+        :data-completed="todo.completed"
+        :class="{ checked: todo.completed }"
+        @click="toggleCompleted(todo)"
+        @dblclick="editTodo(todo)"
+      >
+        <span v-if="editingTodoId !== todo.id">{{ todo.title }}</span>
+        <input
+          v-else
+          type="text"
+          v-model="editedTitle"
+          @blur="saveTodo(todo)"
+          @keyup.enter="saveTodo(todo)"
+          ref="editInput"
+        />
+        <span class="close" @click.stop="deleteTodo(todo.id)">&#xD7;</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from 'vue';
+import { ref, onMounted, watch, nextTick } from 'vue';
 import axios from 'axios';
 
 const todos = ref([]);
 const loading = ref(true);
 const newTodoTitle = ref('');
+const editingTodoId = ref(null);
+const editedTitle = ref('');
+const editInput = ref(null);
 
 onMounted(() => {
-	fetchTodos();
+  fetchTodos();
 });
 
 const fetchTodos = () => {
-	loading.value = true;
-	axios.get('http://localhost:8000/api/todos')
-		.then(response => {
-			todos.value = response.data;
-			nextTick(() => {
-				loading.value = false;
-			});
-		})
-		.catch(error => {
-			console.error('Error fetching todos:', error);
-			nextTick(() => {
-				loading.value = false;
-			});
-		});
+  loading.value = true;
+  axios.get('http://localhost:8000/todos')
+    .then(response => {
+      todos.value = response.data;
+      nextTick(()=>{loading.value = false;});
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+      nextTick(()=>{loading.value = false;});
+    });
 };
 
 const addTodo = () => {
-	if (newTodoTitle.value.trim()) {
-		axios.post('http://localhost:8000/api/todos', { title: newTodoTitle.value })
-			.then(() => {
-				newTodoTitle.value = '';
-				fetchTodos();
-			})
-			.catch(error => {
-				console.error('Error adding todo:', error);
-			});
-	}
+  if (newTodoTitle.value.trim()) {
+    axios.post('http://localhost:8000/todos', { title: newTodoTitle.value })
+      .then(() => {
+        newTodoTitle.value = '';
+        fetchTodos();
+      })
+      .catch(error => {
+        console.error('Error adding todo:', error);
+      });
+  }
 };
 
-const updateTodo = (todo) => {
-	axios.put(`http://localhost:8000/api/todos/${todo.id}`, todo)
-		.catch(error => {
-			console.error('Error updating todo:', error);
-		});
+const toggleCompleted = (todo) => {
+  axios
+    .put(`http://localhost:8000/todos/${todo.id}`, {
+      ...todo,
+      completed: !todo.completed,
+    })
+    .then(() => {
+      fetchTodos();
+    })
+    .catch((error) => {
+      console.error('Error updating todo:', error);
+    });
 };
 
 const deleteTodo = (id) => {
-	axios.delete(`http://localhost:8000/api/todos/${id}`)
-		.then(() => {
-			fetchTodos();
-		})
-		.catch(error => {
-			console.error('Error deleting todo:', error);
-		});
+  axios
+    .delete(`http://localhost:8000/todos/${id}`)
+    .then(() => {
+      fetchTodos();
+    })
+    .catch((error) => {
+      console.error('Error deleting todo:', error);
+    });
+};
+
+const editTodo = (todo) => {
+  editingTodoId.value = todo.id;
+  editedTitle.value = todo.title;
+  nextTick(() => {
+    if (editInput.value) {
+      editInput.value.focus();
+    }
+  });
+};
+
+const saveTodo = (todo) => {
+  editingTodoId.value = null;
+  axios
+    .put(`http://localhost:8000/todos/${todo.id}`, {
+      ...todo,
+      title: editedTitle.value,
+    })
+    .then(() => {
+      fetchTodos();
+    })
+    .catch((error) => {
+      console.error('Error updating todo:', error);
+    });
 };
 </script>
